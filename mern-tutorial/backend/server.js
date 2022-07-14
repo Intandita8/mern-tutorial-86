29c0b0cf106b7e414f8359b9dad55253b1283477
intanpradita <intanpn086@gmail.com>
2022-07-14 15:35:39 +0700

    membuat route

--- mern-tutorial/backend/server.js
+++ mern-tutorial/backend/server.js
@@ -6,3 +6,6 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
+app.get('/api/goals',(req,res)=>{
+    res.json({message: 'Get Goals'})
+})
